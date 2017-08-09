2c5d4595afefeb10e75377a8919c8d6a7972df18
Fausto Oliveira <fausto.oliveira@volvo.com>
2017-08-08 23:52:05 -0400

    Implemented 1.2 Permutation - O(N log N) and O(N)

--- python/cracking-code-interview/1_2_permutation.py
+++ python/cracking-code-interview/1_2_permutation.py
@@ -1,20 +1,72 @@
-def permutation_o_n_square():
-    equals = 0
-    for ci in string_1:
-        for cj in string_2:
-            if ci == cj:
-                equals += 1
-    return equals == len(string_1)
+import unittest
 
 
-string_1 = input("Inform the first String: ")
-string_2 = input("Inform the second String: ")
+def permutation_o_n_log_n(s1, s2):
+    if len(s1) != len(s2):
+        return False
 
-if len(string_1) != len(string_2):
-    print("Both Strings should have the same length, doh!")
-    exit(1)
+    search_indexes = list(range(len(s1)))
+    for c in s2:
+        found = False
+        for i in range(len(search_indexes)):
+            if s1[search_indexes[i]] == c:
+                del search_indexes[i]
+                found = True
+                break
+        if not found:
+            return False
+    return True
 
-if permutation_o_n_square():
-    print("The second String IS a permutation of the first")
-else:
-    print("The second String is NOT a permutation of the first")
+
+def permutation_o_n(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    hash_table = []
+    for i in range(20):
+        hash_table.append([])
+
+    for c in s1:
+        hash_code = ord(c) % len(hash_table)
+        hash_list = hash_table[hash_code]
+        hash_list.append(c)
+
+    for c in s2:
+        hash_code = ord(c) % len(hash_table)
+        hash_list = hash_table[hash_code]
+        found = False
+        for i in range(len(hash_list)):
+            if c == hash_list[i]:
+                del hash_list[i]
+                found = True
+                break
+        if not found:
+            return False
+
+    return True
+
+
+class TestFunctions(unittest.TestCase):
+    def test_o_n_log_n(self):
+        self.assertTrue(permutation_o_n_log_n("asd", "asd"))
+        self.assertTrue(permutation_o_n_log_n("asd", "ads"))
+        self.assertTrue(permutation_o_n_log_n("asd", "das"))
+        self.assertTrue(permutation_o_n_log_n("asd", "sad"))
+        self.assertTrue(permutation_o_n_log_n("asd", "sda"))
+        self.assertTrue(permutation_o_n_log_n("asd", "dsa"))
+        self.assertFalse(permutation_o_n_log_n("asd", "qwe"))
+        self.assertFalse(permutation_o_n_log_n("asd", "aad"))
+        self.assertFalse(permutation_o_n_log_n("asd", "aaa"))
+        self.assertFalse(permutation_o_n_log_n("asd", "aaaa"))
+
+    def test_o_n(self):
+        self.assertTrue(permutation_o_n("asd", "asd"))
+        self.assertTrue(permutation_o_n("asd", "ads"))
+        self.assertTrue(permutation_o_n("asd", "das"))
+        self.assertTrue(permutation_o_n("asd", "sad"))
+        self.assertTrue(permutation_o_n("asd", "sda"))
+        self.assertTrue(permutation_o_n("asd", "dsa"))
+        self.assertFalse(permutation_o_n("asd", "qwe"))
+        self.assertFalse(permutation_o_n("asd", "aad"))
+        self.assertFalse(permutation_o_n("asd", "aaa"))
+        self.assertFalse(permutation_o_n("asd", "aaaa"))

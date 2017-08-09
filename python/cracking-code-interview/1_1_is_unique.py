744d9bf89a6ffa3375ff39300ba59a2ae52f4c44
Fausto Oliveira <fausto.oliveira@volvo.com>
2017-08-08 22:33:39 -0400

    Implemented 1.1 Is Unique - O(N) using hash

--- python/cracking-code-interview/1_1_is_unique.py
+++ python/cracking-code-interview/1_1_is_unique.py
@@ -1,14 +1,36 @@
-def unique_o_square():
+def unique_o_n_square():
     for i in range(len(string)):
         for j in range(len(string)):
             if i != j and string[i] == string[j]:
                 return False
     return True
 
-string = input("Insert string to test: ")
-result_o_square = unique_o_square()
 
-if result_o_square:
+def unique_o_n():
+    hash_table = []
+    for i in range(20):
+        hash_table.append([])
+
+    for c in string:
+        hash_code = ord(c) % len(hash_table)
+        hash_list = hash_table[hash_code]
+        for list_item in hash_list:
+            if c == list_item:
+                return False
+        hash_list.append(c)
+
+    return True
+
+
+string = input("Insert string to test: ")
+result_o_n_square = unique_o_n_square()
+result_o_n = unique_o_n()
+
+if result_o_n != result_o_n_square:
+    print("This bad program could not make its mind, sorry :[")
+    exit(1)
+
+if result_o_n_square:
     print("String contains unique characters")
 else:
     print("String contains repeated characters")

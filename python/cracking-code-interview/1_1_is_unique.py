67a349d7483deaa7e87a57b2dbf1d22780da1461
Fausto Oliveira <fausto.oliveira@volvo.com>
2017-08-08 21:53:16 -0400

    Implemented 1.1 Is Unique - O(n^2)

--- python/cracking-code-interview/1_1_is_unique.py
+++ python/cracking-code-interview/1_1_is_unique.py
@@ -1,9 +1,14 @@
+def unique_o_square():
+    for i in range(len(string)):
+        for j in range(len(string)):
+            if i != j and string[i] == string[j]:
+                return False
+    return True
+
 string = input("Insert string to test: ")
+result_o_square = unique_o_square()
 
-for i in range(len(string)):
-    for j in range(len(string)):
-        if i != j and string[i] == string[j]:
-            print("string contains repeated characters")
-            exit(1)
-
-print("string contains unique characters")
+if result_o_square:
+    print("String contains unique characters")
+else:
+    print("String contains repeated characters")

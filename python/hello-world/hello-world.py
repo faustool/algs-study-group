a934dc8c47a0d8a4e1f929dc6efe5202b567d391
Fausto Oliveira <fausto.oliveira@volvo.com>
2017-08-08 21:22:13 -0400

    added intellij project

--- python/hello-world/hello-world.py
+++ python/hello-world/hello-world.py
@@ -1,9 +1,9 @@
 print("Hello, World!")
 
 for i in range(5):
-  print ("Printing 0 to 4")
-  print (i)
-  
+    print("Printing 0 to 4")
+    print(i)
+
 for i in range(3):
-  print ("Printing 0 to 2")
-  print (i)
+    print("Printing 0 to 2")
+    print(i)

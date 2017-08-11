35cd97c0bcf282be72b1fee11d540eccf166fd3e
Fausto Oliveira <fausto.oliveira@volvo.com>
2017-08-10 22:05:16 -0400

    implemented 1.3 urlify with O(N)

--- python/cracking-code-interview/1_3_urlify.py
+++ python/cracking-code-interview/1_3_urlify.py
@@ -1,23 +1,46 @@
 import unittest
 
 
-def urlify_n_square(string, length):
-    changeable = list(string)
-    full_length = len(changeable)
+def urlify_n_square(given_string):
+    string = list(given_string)
+    full_length = len(string)
     i = 0
     while i < full_length:
-        if changeable[i] == ' ':
+        if string[i] == ' ':
             for j in range(full_length - 1, i + 2, -1):
-                changeable[j] = changeable[j - 2]
-            changeable[i] = '%'
-            changeable[i + 1] = '2'
-            changeable[i + 2] = '0'
+                string[j] = string[j - 2]
+            string[i] = '%'
+            string[i + 1] = '2'
+            string[i + 2] = '0'
             i += 3
         else:
             i += 1
-    return ''.join(changeable)
+    return ''.join(string)
+
+
+def urlify_n(given_string, length):
+    string = list(given_string)
+    full_length = len(string)
+    i = full_length - 1
+    j = length - 1
+    while j > 0:
+        if string[j] == ' ':
+            string[i] = '0'
+            string[i - 1] = '2'
+            string[i - 2] = '%'
+            i -= 3
+        else:
+            string[i] = string[j]
+            i -= 1
+        j -= 1
+    return ''.join(string)
 
 
 class TestFunctions(unittest.TestCase):
     def test_book_example(self):
-        self.assertEqual(urlify_n_square('Mr John Smith    ', 13), 'Mr%20John%20Smith')
+        self.assertEqual(urlify_n_square('Mr John Smith    '), 'Mr%20John%20Smith')
+        self.assertEqual(urlify_n('Mr John Smith    ', 13), 'Mr%20John%20Smith')
+
+    def test_long_shot(self):
+        self.assertEqual(urlify_n_square('My        N                '), 'My%20%20%20%20%20%20%20%20N')
+        self.assertEqual(urlify_n('My        N                ', 11), 'My%20%20%20%20%20%20%20%20N')
